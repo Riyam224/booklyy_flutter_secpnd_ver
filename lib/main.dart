c22063cad90e43cc72b98aee1efbccc007bb47cd
Riyam224 <cs50riyam211#gmail.com>
2023-10-17 20:33:53 +0300

    add google fonts to main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:booklyy/constants.dart';
 import 'package:booklyy/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Booklyy());
@@ -16,6 +17,7 @@ class Booklyy extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplashView(),
     );
