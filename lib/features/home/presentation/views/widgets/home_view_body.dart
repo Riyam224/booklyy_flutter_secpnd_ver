12db553d5f417342863fbd2da0e70ea828f9b73b
Riyam224 <cs50riyam211#gmail.com>
2023-10-18 18:17:49 +0300

    add list view to make best seller scrolling

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,13 +1,11 @@
 import 'package:booklyy/core/utils/styles.dart';
 import 'package:booklyy/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:booklyy/features/home/presentation/views/widgets/custom_app_bar.dart';
-
 import 'package:booklyy/features/home/presentation/views/widgets/featuredBookListView.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -27,9 +25,27 @@ class HomeViewBody extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          const BestSellerListViewItem()
+          const BestSellerListView()
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: BestSellerListViewItem(),
+            );
+          }),
     );
   }
 }
