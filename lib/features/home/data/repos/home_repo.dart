d09e98d47c69bc272cbf284152bd6f4a94a21707
Riyam224 <cs50riyam211#gmail.com>
2023-10-20 19:19:29 +0300

    create home_Repo methods and failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:booklyy/core/errors/failures.dart';
+import 'package:booklyy/features/home/data/models/books_model/books_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
   // todo methods
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchFeaturedBooks();
 }
