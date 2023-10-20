d885c321a505fbf90915f4a7cb46de5615679867
Riyam224 <cs50riyam211#gmail.com>
2023-10-20 03:19:01 +0300

    add custom search view

---
+++ lib/features/search/presentation/view/widgets/custom_search_text_field.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        // fillColor: Colors.white,
+        // filled: true,
+        enabledBorder: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+        hintText: 'search',
+        suffixIcon: Opacity(
+            opacity: .7,
+            child:
+                IconButton(onPressed: () {}, icon: const Icon(Icons.search))),
+      ),
+    );
+  }
+// todo method to use border 
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.white,
+      ),
+      borderRadius: BorderRadius.circular(
+        12,
+      ),
+    );
+  }
+}
