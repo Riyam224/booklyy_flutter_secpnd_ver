b88bae8b4b53ed0e528ac18b540068c53f5df07a
Riyam224 <cs50riyam211#gmail.com>
2023-10-21 20:27:30 +0300

    create newest books state cubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -7,9 +7,7 @@ part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
-
   final HomeRepo homeRepo;
-
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
     var result = await homeRepo.fetchFeaturedBooks();
